422333e2dca9eaac1259d07f28d8ac0d82d56bac
Sasank Varanasi <42350947+7thmaniac@users.noreply.github.com>
2024-07-05 11:38:39 +0000

    ui using chainlit

--- chat.py
+++ chat.py
@@ -1,8 +1,10 @@
+import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
+
+# llm = AutoModelForCausalLM.from_pretrained(
+#     "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+# )
 
 prompt = "capital city of India"
 
@@ -11,14 +13,49 @@ prompt = "capital city of India"
 
 def get_prompt(instruction: str, history: list[str] | None = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in short and concise way"
-    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    prompt = f"### System:\n{system}\n\n### User:"
+    if history is not None:
+        prompt += f"This is conversation history: {''.join(history)}. Now answer the question."
+    prompt += f"\n{instruction}\n\n### Response:\n"
     print(prompt)
     return prompt
 
 
+@cl.on_message
+async def main(message: cl.Message):
+    prompt = get_prompt(message.content)
+    response = llm(prompt)
+    await cl.Message(
+        response
+    ).send()
+
+
+@cl.on_chat_start
+def on_chat_start():
+    print("A new chat session has started!")
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+
+
+'''
+history = []
+
 Question = "Which city is the capital of India?"
 
+answer = ''
 
 for word in llm(get_prompt(Question), stream=True):
     print(word, end='', flush=True)
+    answer += word
 print()
+
+history.append(answer)
+
+Question = "And which is of the United States?"
+
+for word in llm(get_prompt(Question, history), stream=True):
+    print(word, end='', flush=True)
+print()
+'''
